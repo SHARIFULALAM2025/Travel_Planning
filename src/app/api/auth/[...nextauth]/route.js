65297a951d0bb994af59875bd102d37e49f307fa
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2026-02-22 13:38:57 +0600

    shariful implement login-signup-logout-auth-system update

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -17,7 +17,7 @@ export const authOptions = {
                 return null;
             }
         }),
-        // social login
+        // social login provider
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
