f2664a62fba34b52d7df6946115841e6e52d1844
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2026-03-02 21:45:30 +0600

    navbar responsive

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,10 @@ const nextConfig = {
       protocol: 'https',
       hostname: 'www.svgrepo.com',
     },
+    {
+      protocol: 'https',
+      hostname: 'lh3.googleusercontent.com',
+    },
   ],
   },
 };
