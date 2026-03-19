6536fcfd776d745249af97a77d37c117187af748
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2026-03-19 09:58:54 +0600

    order page add

---
+++ src/app/[locale]/(dashboard)/order/page.jsx
@@ -0,0 +1,12 @@
+import MyOrder from '@/Components/MainDashboard/Order/MyOrder';
+import React from 'react';
+
+const OrderPage = () => {
+    return (
+        <div>
+            <MyOrder/>
+        </div>
+    );
+};
+
+export default OrderPage;
