f10b7224142d9fbab7ffca104232588b0c3b9385
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2026-03-29 12:00:07 +0600

    hotel add

--- src/app/[locale]/(marketing)/explore/page.jsx
+++ src/app/[locale]/(marketing)/explore/page.jsx
@@ -1,14 +1,20 @@
+import Bus from '@/Components/Explore_DestinationPages/Bus/Bus';
+import Cruise from '@/Components/Explore_DestinationPages/Cruise/Cruise';
 import HeroExplore from '@/Components/Explore_DestinationPages/HeroUi/HeroExplore';
+import Hotel from '@/Components/Explore_DestinationPages/Hotel/Hotel';
 import Trending from '@/Components/Explore_DestinationPages/Trending/Trending';
-
+import Visa from '@/Components/Explore_DestinationPages/Visa/Visa';
 import React from 'react';
-
 const ExplorePage = () => {
     return (
         <div>
 
             <HeroExplore />
-            <Trending/>
+            <Trending />
+            <Bus />
+            <Cruise />
+            <Visa />
+            <Hotel/>
 
         </div>
     );
