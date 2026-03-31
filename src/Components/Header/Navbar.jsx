685e4aa50e4aabadf2478708dd98c8255d7407db
SHARIFULALAM2025 <sharifullinkdin2025@gmail.com>
2026-04-01 03:23:05 +0600

    navbar and contact page correction

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -117,6 +117,17 @@ const Navbar = () => {
   }
 };
   //
+  const bgStyle =
+        theme === 'dark'
+          ? {
+            backgroundColor: '#0F172A',
+
+              backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%23334155' fill-opacity='0.2' d='M1 3h1v1H1V3zm2-2h1v1H2V1z'%3E%3C/path%3E%3C/svg%3E")`,
+            }
+          : {
+            backgroundColor: '#FFFFFF',
+
+            }
 
   useEffect(() => {
     setMounted(true)
@@ -154,6 +165,7 @@ const Navbar = () => {
   return (
     <Container>
       <nav
+        style={bgStyle}
         className={`sticky top-0 z-50 transition-all duration-300 ${
           theme === 'dark'
             ? 'bg-slate-900 text-white'
